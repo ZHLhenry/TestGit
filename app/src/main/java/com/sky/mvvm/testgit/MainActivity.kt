707600bb5cac25e12be5f1001bc2df0c19a2b5eb
henry <524934786@qq.com>
2025-06-15 10:12:35 +0800

    mainactivity update

--- app/src/main/java/com/sky/mvvm/testgit/MainActivity.kt
+++ app/src/main/java/com/sky/mvvm/testgit/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.sky.mvvm.testgit.ui.theme.TestGitTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding).padding(all = 10.dp)
                     )
                 }
             }
